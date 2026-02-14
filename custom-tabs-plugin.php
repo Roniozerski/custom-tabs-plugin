3c8c0647680ad0f0be13ccd74251b9094760b6d7
Roniozerski <roni.ozerski@gmail.com>
2026-02-14 11:58:07 +0200

    feat: scaffold plugin bootstrap file

---
+++ custom-tabs-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Custom Tabs Plugin
+ * Description: Responsive tabs component powered by ACF.
+ * Version: 1.0.0
+ * Author: Roni Ozerski
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
